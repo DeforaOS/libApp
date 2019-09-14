28faa2762a34af41b9ff3b97f8798ef1a7f5561c
Pierre Pronchery <khorben@defora.org>
2019-09-14 04:32:18 +0200

    Import the latest versions of the scripts

--- tests/appbroker.sh
+++ tests/appbroker.sh
@@ -1,42 +1,97 @@
 #!/bin/sh
 #$Id$
-#Copyright (c) 2016-2017 Pierre Pronchery <khorben@defora.org>
-#This file is part of DeforaOS System libApp
-#This program is free software: you can redistribute it and/or modify
-#it under the terms of the GNU General Public License as published by
-#the Free Software Foundation, version 3 of the License.
+#Copyright (c) 2011-2017 Pierre Pronchery <khorben@defora.org>
 #
-#This program is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU General Public License for more details.
+#Redistribution and use in source and binary forms, with or without
+#modification, are permitted provided that the following conditions are met:
 #
-#You should have received a copy of the GNU General Public License
-#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+#THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+#AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+#DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+#FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+#DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+#SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+#CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+#OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+#OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
 
 #variables
 PROGNAME="appbroker.sh"
 #executables
-APPBROKER="$OBJDIR../tools/AppBroker$EXEEXT"
-DIFF="diff"
+APPBROKER="AppBroker"
 DEBUG="_debug"
-MKTEMP="mktemp"
-RM="rm -f"
 
 
 #functions
+#appbroker
+_appbroker()
+{
+	target="$1"
+	appinterface="$2"
+
+	$DEBUG $APPBROKER -o "$target" "$appinterface"
+}
+
+
+#debug
+_debug()
+{
+	echo "$@" 1>&3
+	"$@"
+}
+
+
+#usage
+_usage()
+{
+	echo "Usage: $PROGNAME [-c] target..." 1>&2
+	return 1
+}
+
+
 #main
-tmpfile=$($MKTEMP)
-[ $? -eq 0 ]							|| exit 2
-interface="$1"
-expected="${interface%.interface}.expected"
-$APPBROKER -o "$tmpfile" "$interface"
-ret=$?
-if [ $ret -eq 0 ]; then
-	$DIFF -- "$expected" "$tmpfile"
-	ret=$?
+clean=0
+while getopts "cO:P:" name; do
+	case "$name" in
+		c)
+			clean=1
+			;;
+		O)
+			export "${OPTARG%%=*}"="${OPTARG#*=}"
+			;;
+		P)
+			#XXX ignored for compatibility
+			;;
+		?)
+			_usage
+			exit $?
+			;;
+	esac
+done
+shift $((OPTIND - 1))
+if [ $# -lt 1 ]; then
+	_usage
+	exit $?
 fi
-$RM -- "$tmpfile"
-exit $ret
+
+[ "$clean" -ne 0 ] && exit 0
+
+exec 3>&1
+while [ $# -gt 0 ]; do
+	target="$1"
+	shift
+
+	source="${target#$OBJDIR}"
+	appinterface="${source##*/}"
+	appinterface="../data/${appinterface%%.h}.interface"
+	_appbroker "$target" "$appinterface"			|| exit 2
+done
